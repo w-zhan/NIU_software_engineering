d41492d7b92d05ab349c8056811ff35a15c352ad
micocho <yuchiehcho@gmail.com>
2025-01-01 22:02:45 +0800

    Update README

--- code.py
+++ code.py
@@ -1,4 +1,12 @@
 a = int(input("enter a : ")
 b = int(input("enter b : ")
 print(a+b)
-
+print("Happy New Year! 2025")
+Mico@MicosLAP MINGW32 ~/Git_exercise/NIU_software_engineering (main)
+Mico@MicosLAP MINGW32 ~/Git_exercise/NIU_software_engineering (main)
+$ git remote -v
+origin  https://github.com/micocho/NIU_software_engineering.git (fetch)
+origin  https://github.com/micocho/NIU_software_engineering.git (push)
+$ git remote -v
+origin  https://github.com/micocho/NIU_software_engineering.git (fetch)
+origin  https://github.com/micocho/NIU_software_engineering.git (push)
